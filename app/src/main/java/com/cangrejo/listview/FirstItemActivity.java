6433d775060792ea72d408c293e972c0da8cc826
Gustavo Gamboa <gustavo.gamboa96@gmail.com>
2019-01-25 15:26:36 -0500

    <first complete commit>

---
+++ app/src/main/java/com/cangrejo/listview/FirstItemActivity.java
@@ -0,0 +1,57 @@
+package com.cangrejo.listview;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.support.v7.widget.Toolbar;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class FirstItemActivity extends YouTubeBaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_item);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp);
+        toolbar.setTitle(getString(R.string.app_name));
+        toolbar.setTitleTextColor(Color.WHITE);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+       });
+
+
+
+        YouTubePlayerView youTubePlayerView =findViewById(R.id.player);
+
+        youTubePlayerView.initialize("AIzaSyAvhYi0gZIftMV8ZylF-jVn-jKJrhlqX9o",
+                new YouTubePlayer.OnInitializedListener() {
+                    @Override
+                    public void onInitializationSuccess(YouTubePlayer.Provider provider,
+                                                        YouTubePlayer youTubePlayer, boolean b) {
+
+                        // do any work here to cue video, play video, etc.
+                        youTubePlayer.cueVideo("9bZkp7q19f0");
+                    }
+                    @Override
+                    public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                                        YouTubeInitializationResult youTubeInitializationResult) {
+
+                    }
+                });
+
+    }
+}
+
---
+++ app/src/main/java/com/cangrejo/listview/MainActivity.java
@@ -0,0 +1,54 @@
+package com.cangrejo.listview;
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       ListView listView=(ListView)findViewById(R.id.listView);
+
+       final String[] Names = {"ejemplo 1", "ejemplo 2", "ejemplo 3", "15", "fvfv","fvf","vfvf","vffvfv","vfv","hey", "hey", "vvv","hey"};
+       ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Names){
+           public View getView(int position, View convertView, ViewGroup parent){
+               /// Get the Item from ListView
+               View view = super.getView(position, convertView, parent);
+
+               TextView tv = (TextView) view.findViewById(android.R.id.text1);
+
+               // Set the text size 25 dip for ListView each item
+               tv.setTextSize(TypedValue.COMPLEX_UNIT_DIP,40);
+
+               // Return the view
+               return view;
+           }
+       };
+       listView.setAdapter(adapter);
+
+       listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+           @Override
+           public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+               //String mynames = names[position];
+               if(position == 2){
+                   startActivity(new Intent(MainActivity.this, FirstItemActivity.class));
+                    overridePendingTransition(R.anim.leftright, R.anim.leftright);
+               }
+           }
+       });
+
+    }
+}
